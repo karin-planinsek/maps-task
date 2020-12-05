209c969e0232f0fa4b12a6094528311c2c06c07b
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2020-12-05 19:30:15 +0100

    Creating google-maps component and installing geolocation package

--- google-maps-app/src/app/app.module.ts
+++ google-maps-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GoogleMapsComponent } from './google-maps/google-maps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapsComponent
   ],
   imports: [
     BrowserModule
