564f818cd98b37c6e55c74d6e0315c67598ebf9d
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2020-12-05 19:19:05 +0100

    Adding basic html template and getting user input

--- google-maps-app/src/app/app.component.ts
+++ google-maps-app/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'google-maps-app';
+
+  getLocationInput(location: HTMLInputElement) {
+    console.log(location.value);
+  }
 }
