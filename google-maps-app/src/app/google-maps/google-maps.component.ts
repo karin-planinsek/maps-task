90d42314196143cd0eecc735dc8d6f52a7f34600
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2020-12-06 14:04:27 +0100

    Getting user location upon app initialization and displaying it in google maps

--- google-maps-app/src/app/google-maps/google-maps.component.ts
+++ google-maps-app/src/app/google-maps/google-maps.component.ts
@@ -1,4 +1,9 @@
+/// <reference types="googlemaps" />
+
 import { Component, OnInit } from '@angular/core';
+import { GeolocationService } from '@ng-web-apis/geolocation';
+
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-google-maps',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./google-maps.component.css']
 })
 export class GoogleMapsComponent implements OnInit {
+  latLng;
 
-  constructor() { }
+  constructor(private geolocation: GeolocationService) {
 
-  ngOnInit(): void {
+  }
+
+  ngOnInit() {
+    // get user location and display it in google map
+    this.geolocation.pipe(take(1))
+    .subscribe(position => this.initMap(position.coords.latitude, position.coords.longitude)
+    );
+
+  }
+
+  /*getUserLocation(latitude, longitude) {
+    this.latLng = {lat: latitude, lng: longitude};
+    console.log(this.latLng);
+  }*/
+
+  initMap(latitude, longitude) {
+    // create map
+    let map = new google.maps.Map(
+      document.getElementById('map'),
+      {
+        zoom: 15,
+        center: {lat: latitude, lng: longitude}
+      }
+    );
+
+    console.log('Map initialized');
+
+    // create marker on map
+    let marker = new google.maps.Marker({
+      position: {lat: latitude, lng: longitude},
+      map,
+      title: 'My Location'
+    });
   }
 
 }
