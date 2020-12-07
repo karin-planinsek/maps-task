5c61b51a33fdd9b1bc3c3e4940a557cb34ef79d3
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2020-12-07 14:24:41 +0100

    Displaying 8 - day weather forecast for current user location, getting user address with reverse geocoding

--- google-maps-app/src/app/weather/weather.component.ts
+++ google-maps-app/src/app/weather/weather.component.ts
@@ -1,7 +1,10 @@
+/// <reference types="googlemaps" />
+
 import { Component, OnInit } from '@angular/core';
 import { GeolocationService } from '@ng-web-apis/geolocation';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { take } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-weather',
@@ -9,18 +12,39 @@ import { take } from 'rxjs/operators';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-  header = new HttpHeaders({'Content-Type': 'application/json'});
   API_KEY = '15b0f55ea5c54b159449393e829e0fa5';
+
+  address;
+
+  weatherToday;
+  days = [];
 
   constructor(private geolocation: GeolocationService,
               private http: HttpClient) { }
 
   ngOnInit(): void {
+    let today = new Date();
+
+    for (let i = 0; i < 8; i++) {
+      let nexDay = new Date();
+      nexDay.setDate(today.getDate() + i);
+      this.days.push(nexDay.toString().substring(0, 10));
+    }
+
+    console.log(this.days);
+
     this.geolocation.pipe(take(1))
     .subscribe(position => {
+      this.getAddress(position.coords.latitude, position.coords.longitude);
       this.getWeather(position.coords.latitude, position.coords.longitude)
       .subscribe(res => {
-        console.log(res);
+        console.log(res['daily']);
+        this.weatherToday = res['daily'];
+        for (let i = 0; i < this.weatherToday.length; i++) {
+          let weather = this.weatherToday[i];
+          weather.date = this.days[i];
+        }
+        console.log(this.weatherToday);
       });
     });
   }
@@ -28,6 +52,26 @@ export class WeatherComponent implements OnInit {
   getWeather(lat: number, lng: number) {
     return this.http.get(`
     https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${this.API_KEY}`);
+  }
+
+  getAddress(lat: number, lng: number) {
+    let geocoder = new google.maps.Geocoder();
+    let latLng = new google.maps.LatLng(lat, lng);
+
+    geocoder.geocode(
+      { location: latLng },
+      (
+        results: google.maps.GeocoderResult[],
+        status: google.maps.GeocoderStatus
+      ) => {
+        if (status === 'OK') {
+          this.address = results[0].formatted_address;
+          console.log(this.address);
+        } else {
+          console.log('Geocoding failed');
+        }
+      }
+    );
   }
 
 }
